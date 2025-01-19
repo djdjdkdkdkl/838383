f860e4cccb5eb3b0e5768d6d45f37ab2ce66bb9f
Newyoti <newlohcy@gmail.com>
2025-01-19 16:02:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -153,7 +153,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
                 "-----------------------------------\n"  
                 " Your subscription is active until:\n"  
                 f"       <b>{formatted_expiry}</b> 📅\n"  
-                "If you are Facing Any Problem Then Contact the Admin @TheZidx",  
+                "If you are Facing Any Problem Then Contact the Admin @SPIDERTISEAD",  
                 reply_markup=reply_markup,  
                 parse_mode="HTML"  
             )  
